1e40d7e496ea35e4504d16b5ece3279c1c7f38ad
Alyxia Sother <lexisoth2005@gmail.com>
2021-11-11 16:09:10 +0100

    [Home] Improve cards

--- src/Home.tsx
+++ src/Home.tsx
@@ -11,6 +11,7 @@ export const query = graphql`
                     description
                     creator {
                         name
+                        avatarUrl
                     }
                     identifier
                 }
@@ -28,14 +29,19 @@ export default function HomePage({data}: HomePageProps): JSX.Element {
             <div className="itemgrid">
                 {data.linear.issues.nodes.map((issue) => (
                     <div className="griditem">
-                        <div className="item-name">
-                            <a href={`/issues/${issue.identifier}`}>{issue.title}</a>
-                        </div>
-                        <div className="item-desc">
-                            {issue.description ? issue.description : <em>No description.</em>}
+                        <div className="item-meta">
+                            <div className="item-name">
+                                <a href={`/issues/${issue.identifier}`}>{issue.title}</a>
+                            </div>
+                            <div className="item-desc">
+                                {issue.description ? issue.description : <em>No description.</em>}
+                            </div>
                         </div>
                         <div className="item-spacer"></div>
-                        <div className="item-footer">{issue.creator!.name}</div>
+                        <div className="item-footer">
+                            <img className="item-footer-image" src={issue.creator!.avatarUrl} alt={`${issue.creator!.name}'s profile picture`} />
+                            {issue.creator!.name}
+                        </div>
                     </div>
                 ))}
             </div>
